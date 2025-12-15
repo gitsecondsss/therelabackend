e406a826a90630cfd5811a2e32138d850069f885
gitsecondsss <gitsecondsss@gmx.us>
2025-12-15 23:50:13 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 define('TOKEN_SECRET', 'ENZBzBul6cfevzAENZBzBul6cfevzA'); // keep secret
 define('TOKEN_TTL', 300); // 5 minutes
 define('ALLOWED_ORIGINS', [
-    'https://your-zoho-captcha-page.com',
+    'https://portalaccess.zoholandingpage.com',
 ]);
 
 define('RATE_LIMIT_DIR', __DIR__ . '/data/ratelimit');
